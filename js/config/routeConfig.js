be7f751fcf9d966175fd961a0ef37f26ce9d58bf
Coulthurst Schmitt <ctschmitt21@gmail.com>
2016-01-28 18:03:55 -0600

    added mainController

--- js/config/routeConfig.js
+++ js/config/routeConfig.js
@@ -2,7 +2,7 @@ angular.module('Jango').config(['$routeProvider', function ($routeProvider) {
     $routeProvider
     .when("/", {
         templateUrl: "partials/home.html",
-        controller: "ViewTopicController"
+        controller: "MainController"
     })
     .when("/aww", {
         templateUrl: "partials/posts.html",
---
+++ js/controllers/mainController.js
@@ -0,0 +1,16 @@
+
+(function () {
+    'use strict';
+
+
+    angular.module('Jango').controller('MainController', ['$scope', '$mdSidenav', '$firebaseObject',
+        function ($scope, $mdSidenav, $firebaseObject) {
+
+    $scope.toggleSidenav = function (menuId) {
+        $mdSidenav(menuId).toggle();
+    }
+   
+        
+}]);
+
+angular.module('Jango').run();
